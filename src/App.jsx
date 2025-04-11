3660e51bf0b5f0df72c954d77e7a0f6f75386492
SamirHossain2001 <2001samirhossain@gmail.com>
2025-04-11 08:40:35 +0600

    _

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
 import Navbar from './components/Navbar/Navbar'
 
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Blogs></Blogs>
     </>
   )
 }
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Blogs = () => {
+    
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(()=> {
+        fetch("blogs.json")
+        .then(res => res.json())
+        .then(data => setBlogs(data));
+    }
+    ,[])
+    console.log(blogs);
+    
     return (
         <div>
             
